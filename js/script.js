362299545ac0f480b6d5483cd11c6287bd5b2669
murin.89@list.ru <murin.89@list.ru>
2023-07-28 15:44:01 +0400

    add new

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@ $(function() {
 		fixedContentPos: false
 	});
 });
+
+const contenTimeCodeShow = document.querySelector("#content__time-code-show");
+const contenTimeCodeClose = document.querySelector(".content__time-code-close");
+const contentTimeCodeBoxBody = document.querySelector(".content__time-code-box-body");
+
+contenTimeCodeShow.addEventListener("click", function() {
+	openContent();
+})
+contenTimeCodeClose.addEventListener("click", function() {
+	closeContent();
+})
+function openContent(){
+	contentTimeCodeBoxBody.classList.remove("none");
+	contenTimeCodeClose.classList.remove("none");
+	contenTimeCodeShow.classList.add("none");
+}
+
+function closeContent(){
+	contentTimeCodeBoxBody.classList.add("none");
+	contenTimeCodeClose.classList.add("none");
+	contenTimeCodeShow.classList.remove("none");
+}
+
