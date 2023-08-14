1b31e5efbdd2ee57cbfb0a0ea9ef40a1db9f1e21
murin.89@list.ru <murin.89@list.ru>
2023-08-14 10:17:51 +0400

    media 786 settings

--- js/slider.js
+++ js/slider.js
@@ -1,22 +1,33 @@
 
 $('.slider__top').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    autoplay: false,
-    autoplaySpeed: 4000,
-    asNavFor: '.slider__buttom'
-  });
-  $('.slider__buttom').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    asNavFor: '.slider__top',
-    arrows: false,
-    dots: false,
-    centerMode: true,
-    focusOnSelect: true
-  });
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  autoplay: false,
+  autoplaySpeed: 4000,
+  asNavFor: '.slider__buttom'
+});
+
+$('.slider__buttom').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider__top',
+  arrows: false,
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 786,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
 // ====================
   // $('.nextPage__row').slick({
   //   infinite: true,
