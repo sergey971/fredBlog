b832bf3f56328234c8d6df8fc2b9bebf103d6310
murin.89@list.ru <murin.89@list.ru>
2023-08-30 13:51:26 +0400

    add scrool

--- js/script.js
+++ js/script.js
@@ -1,45 +1,45 @@
-$(function() {
-	$('.video__popup__link').magnificPopup({
-		disableOn: 700,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
+$(function () {
+    $('.video__popup__link').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
 
-		fixedContentPos: false
-	});
+        fixedContentPos: false
+    });
 });
 // slider
-$(document).ready(function(){
+$(document).ready(function () {
     const owl = $('.owl-carousel');
     owl.owlCarousel({
-        rewindNav:false,
-                loop: true,
-                nav: false,
-                dots: false,
-                responsive: {
-                    0:{
-                        items: 1
-                    },
-                    600: {
-                        items: 3,
+        rewindNav: false,
+        loop: true,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3,
 
-                    },
-                    900: {
-                        items: 2
-                    },
-                    1000: {
-                        items: 4
-                    },
-                    1024: {
-                        items: 4
-                    },
-                    1140: {
-                        items: 5
-                    }
-                }
-            })
-            $('.slider__btn').click(function() {
+            },
+            900: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            },
+            1024: {
+                items: 4
+            },
+            1140: {
+                items: 5
+            }
+        }
+    })
+    $('.slider__btn').click(function () {
         owl.trigger('next.owl.carousel');
     });
 });
@@ -57,61 +57,61 @@ const contentRight = document.querySelector(".boxRight__body")
 // const colorsBg = document.querySelectorAll('.colors');
 
 
-contenTimeCodeShow.addEventListener("click", function() {
-	openContent();
+contenTimeCodeShow.addEventListener("click", function () {
+    openContent();
 })
-contenTimeCodeClose.addEventListener("click", function() {
-	closeContent();
+contenTimeCodeClose.addEventListener("click", function () {
+    closeContent();
 })
-function openContent(){
-	contentTimeCodeBoxBody.classList.remove("none");
-	contenTimeCodeClose.classList.remove("none");
-	contenTimeCodeShow.classList.add("none");
+function openContent() {
+    contentTimeCodeBoxBody.classList.remove("none");
+    contenTimeCodeClose.classList.remove("none");
+    contenTimeCodeShow.classList.add("none");
 }
 
-function closeContent(){
-	contentTimeCodeBoxBody.classList.add("none");
-	contenTimeCodeClose.classList.add("none");
-	contenTimeCodeShow.classList.remove("none");
+function closeContent() {
+    contentTimeCodeBoxBody.classList.add("none");
+    contenTimeCodeClose.classList.add("none");
+    contenTimeCodeShow.classList.remove("none");
 }
 
-btnTop.addEventListener("click",goTop);
-window.addEventListener("scroll",trackScroll);
-function goTop(){
-	if(window.window.pageYOffset > 0){
-		window.scrollBy(0, -75);
-		setTimeout(goTop, 10);
-	}
+btnTop.addEventListener("click", goTop);
+window.addEventListener("scroll", trackScroll);
+function goTop() {
+    if (window.window.pageYOffset > 0) {
+        window.scrollBy(0, -75);
+        setTimeout(goTop, 10);
+    }
 }
-function trackScroll(){
-	const offSet = window.pageYOffset;
-	const coords = document.documentElement.clientHeight;  // Вычесляем высоту окна браузера
-	if(offSet > coords){
-		btnTop.classList.add("top--show");
-	}
-	else{
-		
-		btnTop.classList.remove("top--show");
-	}
+function trackScroll() {
+    const offSet = window.pageYOffset;
+    const coords = document.documentElement.clientHeight;  // Вычесляем высоту окна браузера
+    if (offSet > coords) {
+        btnTop.classList.add("top--show");
+    }
+    else {
 
+        btnTop.classList.remove("top--show");
+    }
 }
 // ========================================= СКРОЛ
-// function scrollRight() {
-// 	const yOffset = 450;
-  
-// 	function handleScroll() {
-// 	  const scrollOffset = window.pageYOffset;
-// 	  if (scrollOffset >= yOffset) {
-// 		contentRight.style.cssText = "position: fixed;  bottom: 0px; height: 100%";
-// 	  } else {
-// 		contentRight.style.position = "static";
-// 	  }
-// 	}
-  
-// 	window.addEventListener("scroll", handleScroll);
-//   }
-  
-//   scrollRight();
+function scrollRight() {
+    const yOffset = 450;
+    const width = 768;
+    function handleScroll() {
+        const scrollOffset = window.pageYOffset;
+        const widthWin = window.innerWidth;
+        if (scrollOffset >= yOffset && widthWin >= width) {
+            contentRight.style.cssText = `position: fixed; width:100%; bottom: 0px; height: 100%`;
+        } else {
+            contentRight.style.position = "static";
+        }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+}
+
+scrollRight();
 // ===============================================
 
 const colorsBg = document.querySelectorAll('.colors');
